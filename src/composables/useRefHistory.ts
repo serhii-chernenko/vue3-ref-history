42d39fc43a85993c77b0bbf24250a20519f19a91
Serhii Chernenko <serhii.chernenko@vaimo.com>
2024-07-15 14:00:50 +0200

    task 7 done

--- src/composables/useRefHistory.ts
+++ src/composables/useRefHistory.ts
@@ -1,8 +1,69 @@
-import type { Ref } from 'vue'
-export const useRefHistory = (theme: Ref<string>, capacity: Ref<number>) => {
+import { ref, toValue, watch } from 'vue'
+import type { Ref, MaybeRefOrGetter } from 'vue'
+
+interface HistoryItem {
+    value: MaybeRefOrGetter<string>
+    timestamp: number
+}
+
+type HistoryNavigation = () => void
+
+interface RefHistory {
+    undo: HistoryNavigation
+    redo: HistoryNavigation
+    history: Ref<HistoryItem[]>
+}
+
+type RefHistoryFn = (theme: Ref<string>, capacity: Ref<number>) => RefHistory
+
+export const useRefHistory: RefHistoryFn = (theme, capacity) => {
+    const history = ref<HistoryItem[]>([])
+    const result = ref<HistoryItem[]>(history.value)
+    const allowRedo = ref(false)
+
+    const cutResult = () => {
+        allowRedo.value = false
+
+        if (history.value.length <= toValue(capacity)) {
+            result.value = history.value
+        }
+
+        result.value = history.value.slice(-toValue(capacity))
+    }
+
+    watch(
+        theme,
+        () => {
+            history.value = result.value
+
+            history.value.push({
+                value: toValue(theme),
+                timestamp: Date.now(),
+            })
+            cutResult()
+        },
+        {
+            immediate: true,
+        },
+    )
+
+    watch(capacity, () => cutResult())
+
     return {
-        undo: () => {},
-        redo: () => {},
-        history: [],
+        undo: () => {
+            result.value.pop()
+            allowRedo.value = true
+        },
+        redo: () => {
+            const last = history.value.at(-1)
+
+            if (!last || !allowRedo.value) {
+                return
+            }
+
+            result.value.push(last)
+            allowRedo.value = history.value.length > result.value.length
+        },
+        history: result,
     }
 }
